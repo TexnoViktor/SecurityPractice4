7230143d1cbf9c3c527845c02942ed58bf80252a
viktor <ushakovviktor14@gmail.com>
2024-11-01 19:47:10 +0100

    Добавьте файлы проекта.

---
+++ SecurityPractice4/MainForm.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace SecurityPractice4
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonEncrypt = new System.Windows.Forms.Button();
+            this.buttonDecrypt = new System.Windows.Forms.Button();
+            this.comboBoxAlgorithm = new System.Windows.Forms.ComboBox();
+            this.textBoxInput = new System.Windows.Forms.TextBox();
+            this.textBoxOutput = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // buttonEncrypt
+            // 
+            this.buttonEncrypt.Location = new System.Drawing.Point(66, 238);
+            this.buttonEncrypt.Name = "buttonEncrypt";
+            this.buttonEncrypt.Size = new System.Drawing.Size(75, 23);
+            this.buttonEncrypt.TabIndex = 1;
+            this.buttonEncrypt.Text = "Encrypt";
+            this.buttonEncrypt.UseVisualStyleBackColor = true;
+            this.buttonEncrypt.Click += new System.EventHandler(this.buttonEncrypt_Click);
+            // 
+            // buttonDecrypt
+            // 
+            this.buttonDecrypt.Location = new System.Drawing.Point(188, 238);
+            this.buttonDecrypt.Name = "buttonDecrypt";
+            this.buttonDecrypt.Size = new System.Drawing.Size(75, 23);
+            this.buttonDecrypt.TabIndex = 2;
+            this.buttonDecrypt.Text = "Decrypt";
+            this.buttonDecrypt.UseVisualStyleBackColor = true;
+            this.buttonDecrypt.Click += new System.EventHandler(this.buttonDecrypt_Click);
+            // 
+            // comboBoxAlgorithm
+            // 
+            this.comboBoxAlgorithm.FormattingEnabled = true;
+            this.comboBoxAlgorithm.Items.AddRange(new object[] {
+            "DES",
+            "TripleDES",
+            "AES"});
+            this.comboBoxAlgorithm.Location = new System.Drawing.Point(66, 155);
+            this.comboBoxAlgorithm.Name = "comboBoxAlgorithm";
+            this.comboBoxAlgorithm.Size = new System.Drawing.Size(121, 24);
+            this.comboBoxAlgorithm.TabIndex = 3;
+            // 
+            // textBoxInput
+            // 
+            this.textBoxInput.Location = new System.Drawing.Point(66, 64);
+            this.textBoxInput.Name = "textBoxInput";
+            this.textBoxInput.Size = new System.Drawing.Size(331, 22);
+            this.textBoxInput.TabIndex = 4;
+            // 
+            // textBoxOutput
+            // 
+            this.textBoxOutput.Location = new System.Drawing.Point(66, 108);
+            this.textBoxOutput.Name = "textBoxOutput";
+            this.textBoxOutput.Size = new System.Drawing.Size(331, 22);
+            this.textBoxOutput.TabIndex = 5;
+            // 
+            // MainForm
+            // 
+            this.ClientSize = new System.Drawing.Size(449, 343);
+            this.Controls.Add(this.textBoxOutput);
+            this.Controls.Add(this.textBoxInput);
+            this.Controls.Add(this.comboBoxAlgorithm);
+            this.Controls.Add(this.buttonDecrypt);
+            this.Controls.Add(this.buttonEncrypt);
+            this.Name = "MainForm";
+            this.Load += new System.EventHandler(this.MainForm_Load_1);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button buttonEncrypt;
+        private System.Windows.Forms.Button buttonDecrypt;
+        private System.Windows.Forms.ComboBox comboBoxAlgorithm;
+        private System.Windows.Forms.TextBox textBoxInput;
+        private System.Windows.Forms.TextBox textBoxOutput;
+    }
+}
---
+++ SecurityPractice4/MainForm.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace SecurityPractice4
+{
+    public partial class MainForm : Form
+    {
+        private SymmetricAlgorithm cryptoProvider;
+
+        public MainForm()
+        {
+            InitializeComponent();
+            comboBoxAlgorithm.SelectedIndex = 0; // За замовчуванням DES
+        }
+
+        private void buttonEncrypt_Click(object sender, EventArgs e)
+        {
+            SetCryptoProvider();
+            string textToEncrypt = textBoxInput.Text;
+            byte[] encryptedData = EncryptData(textToEncrypt);
+            textBoxOutput.Text = BitConverter.ToString(encryptedData);
+        }
+
+        private void buttonDecrypt_Click(object sender, EventArgs e)
+        {
+            SetCryptoProvider();
+            byte[] dataToDecrypt = ParseHex(textBoxInput.Text);
+            string decryptedData = DecryptData(dataToDecrypt);
+            textBoxOutput.Text = decryptedData;
+        }
+
+        private void SetCryptoProvider()
+        {
+            switch (comboBoxAlgorithm.SelectedItem.ToString())
+            {
+                case "DES":
+                    cryptoProvider = new DESCryptoServiceProvider();
+                    cryptoProvider.Key = Encoding.ASCII.GetBytes("ABCDEFGH");
+                    cryptoProvider.IV = Encoding.ASCII.GetBytes("ABCDEFGH");
+                    break;
+                case "TripleDES":
+                    cryptoProvider = new TripleDESCryptoServiceProvider();
+                    cryptoProvider.Key = Encoding.ASCII.GetBytes("ABCDEFGHIJKLMNOPQRSTUVWX"); // 24-байтовий ключ
+                    cryptoProvider.IV = Encoding.ASCII.GetBytes("12345678");
+                    break;
+                case "AES":
+                    cryptoProvider = new AesCryptoServiceProvider();
+                    cryptoProvider.Key = Encoding.ASCII.GetBytes("ABCDEFGHIJKLMNOP");
+                    cryptoProvider.IV = Encoding.ASCII.GetBytes("12345678ABCDEFGH");
+                    break;
+            }
+            cryptoProvider.Mode = CipherMode.CBC;
+        }
+
+        private byte[] EncryptData(string text)
+        {
+            byte[] data = Encoding.UTF8.GetBytes(text);
+            using (MemoryStream ms = new MemoryStream())
+            using (CryptoStream cs = new CryptoStream(ms, cryptoProvider.CreateEncryptor(), CryptoStreamMode.Write))
+            {
+                cs.Write(data, 0, data.Length);
+                cs.Close();
+                return ms.ToArray();
+            }
+        }
+
+        private string DecryptData(byte[] data)
+        {
+            using (MemoryStream ms = new MemoryStream(data))
+            using (CryptoStream cs = new CryptoStream(ms, cryptoProvider.CreateDecryptor(), CryptoStreamMode.Read))
+            using (StreamReader reader = new StreamReader(cs))
+            {
+                return reader.ReadToEnd();
+            }
+        }
+
+        private byte[] ParseHex(string hex)
+        {
+            string[] hexValues = hex.Split('-');
+            byte[] data = new byte[hexValues.Length];
+            for (int i = 0; i < hexValues.Length; i++)
+            {
+                data[i] = Convert.ToByte(hexValues[i], 16);
+            }
+            return data;
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void MainForm_Load_1(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
